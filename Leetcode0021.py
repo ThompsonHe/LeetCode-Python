0beb6a00ef70589232fffb70144344fc4c439636
ThompsonHen <624578239@qq.com>
2020-09-16 14:05:22 +0800

    update Leetcode0021

--- Leetcode0021.py
+++ Leetcode0021.py
@@ -9,22 +9,46 @@
 输出：1->1->2->3->4->4
 
 """
+
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
-class Solution:
-     def mergeTwoLists(self, l1:ListNode, l2:ListNode) -> ListNode:
-         if not l1:
-             return l2
-         elif not l2:
-             return l1
-         elif l1.val < l2.val:
-             l1.next = self.mergeTwoLists(l1.next, l2)
-             return l1
-         else:
-             l2.next = self.mergeTwoLists(l2.next, l1)
-             return l2
 
-         
+class Solution1:
+    """
+    递归
+    """
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if not l1:
+            return l2
+        elif not l2:
+            return l1
+        elif l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l2.next, l1)
+            return l2
+
+
+class Solution2:
+    """
+    迭代
+    """
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        prehead = ListNode(-1)
+        prev = prehead
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                prev.next = l1
+                l1 = l1.next
+            else:
+                prev.next = l2
+                l2 = l2.next
+            prev = prev.next
+        prev.next = l1 if l1 is not None else l2
+        return prehead.next
