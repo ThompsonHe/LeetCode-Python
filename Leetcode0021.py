341b7b9c3af9e27b40c95f007248926c6c7704d8
ThompsonHen <624578239@qq.com>
2020-09-16 13:45:29 +0800

    update Leetcode0021

--- Leetcode0021.py
+++ Leetcode0021.py
@@ -16,4 +16,15 @@ class ListNode:
 
 class Solution:
      def mergeTwoLists(self, l1:ListNode, l2:ListNode) -> ListNode:
+         if not l1:
+             return l2
+         elif not l2:
+             return l1
+         elif l1.val < l2.val:
+             l1.next = self.mergeTwoLists(l1.next, l2)
+             return l1
+         else:
+             l2.next = self.mergeTwoLists(l2.next, l1)
+             return l2
+
          
